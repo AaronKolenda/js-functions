94551bfe52b439b0bfe4d4226b6a5c8b77fafa70
AaronKolenda <kolenda.a@gmail.com>
2015-02-03 14:50:36 -0500

    changing the second function to include the first

--- public/vowel.js
+++ public/vowel.js
@@ -8,19 +8,6 @@
 */
 
 function isVowel(letter) {
-	/*var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-	 "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-		for (i=0; i < alphabet.length; i++) {
-			if ( letter.toLowerCase() === alphabet[0]  || letter.toLowerCase() ===  alphabet[4] 
-				|| letter.toLowerCase() ===  alphabet[8] || 
-				alphabet[i] ===  alphabet[i] || alphabet[i] === alphabet[i] ) {
-				return true;
-			}
-				return false;
-		}
-*/
-
 
 	if (letter.toLowerCase() === "a" || letter.toLowerCase() === "e" || letter.toLowerCase() === "i" 
 		|| letter.toLowerCase() === "o" || letter.toLowerCase() === "u") {
@@ -37,9 +24,12 @@ function isVowel(letter) {
   Hint: string[0] returns the 1st character in the string
 */
 
+
+/* Below is the function I wrote before I read the directions
+
 function countVowels(word) {
 	var vowelcount = 0;  
-	word = word.toLowerCase(); // The Iron Yard
+	word = word.toLowerCase(); 
 
 	for (i=0; i < word.length; i++) {
 		if (word.charAt(i) === "a" || word.charAt(i) === "e" || word.charAt(i) === "i" 
@@ -49,4 +39,18 @@ function countVowels(word) {
 	}
 	return vowelcount;
 
+}*/
+
+function countVowels(word) {
+	var vowelcount = 0;  
+	word = word.toLowerCase(); 
+
+	for (i=0; i < word.length; i++) {
+		if (isVowel(word[i])) {
+		vowelcount++;
+		}
+	}
+	return vowelcount;
+
 }
+
